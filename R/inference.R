8183ed1b70efaa1e21f2f799a8aad67f67031f86
Tom Elliott <tomelliott90@gmail.com>
2015-03-23 13:28:18 +1300

    printing inference info for num ~ cat

--- R/inference.R
+++ R/inference.R
@@ -10,7 +10,13 @@ inference.inzdot <- function(object, bs, width, ...) {
         "Not yet implemented."
     } else {
         
-        means <- do.call(rbind, lapply(toplot, function(t) mean(t$x)))
+        means <- do.call(rbind, lapply(toplot,
+                                       function(t) {
+                                           if (is.null(t$x))
+                                               NA
+                                           else
+                                               mean(t$x, na.rm = TRUE)
+                                       }))
         
         if (is.null(inf$conf))
             stop("Please specify `inference.type = conf` to get inference information.")
@@ -48,7 +54,17 @@ inference.inzdot <- function(object, bs, width, ...) {
                  out)
         
         if (byFactor) {
-            dat <- do.call(rbind, lapply(names(toplot), function(t) data.frame(x = toplot[[t]]$x, y = t)))
+            ## For x ~ factor, we also include an F test, and multiple comparisons
+            ## (estimates, pvalues, and confidence intervals).
+            
+            dat <- do.call(rbind, lapply(names(toplot),
+                                         function(t) {
+                                             if (is.null(toplot[[t]]$x))
+                                                 NULL
+                                             else
+                                                 data.frame(x = toplot[[t]]$x, y = t)
+                                         }
+                                         ))
             fit <- lm(x ~ y, data = dat)
             
             fstat <- summary(fit)$fstatistic
@@ -60,19 +76,41 @@ inference.inzdot <- function(object, bs, width, ...) {
                               format.pval(pf(fstat[1], fstat[2], fstat[3], lower.tail = FALSE),
                                           digits = 2)))
             
-            out <- c(out, "", Ftest, "", #centerText("* * *", width),
+            out <- c(out, "", Ftest, "",
                      "", "   *** Differences between Group Means (column - row) ***", "")
             
             means <- predict(fit, newdata = data.frame(y = levels(dat$y)))
-            names(means) <- levels(dat$y)
+            names(means) <- LEVELS <- levels(dat$y)
             diffMat <- outer(means, means, function(x, y) y - x)
-            diffMat <- formatTriMat(diffMat, names(means))
-            
-            
-            
+            diffMat <- formatTriMat(diffMat, LEVELS)
+
             out <- c(out,
                      "Estimates", "",
                      apply(diffMat, 1, function(x) paste0("   ", paste(x, collapse = "   "))))
+
+            mc <- try(s20x::multipleComp(fit))
+            if (!inherits(mc, "try-error")) {
+                cimat <- triangularMatrix(LEVELS, mc, "ci")
+                cimat <- formatMat(cimat)
+                
+                out <- c(out, "",
+                         "95% Confidence Intervals (adjusted for multiple comparisons)", "",
+                         apply(cimat, 1, function(x) paste0("   ", paste(x, collapse = "   "))))
+                
+                
+                pmat <- triangularMatrix(LEVELS, mc, "p-values")
+                pmat <- formatMat(pmat, 2)
+                
+                out <- c(out, "",
+                         "P-values", "",
+                         apply(pmat, 1, function(x) paste0("   ", paste(x, collapse = "   "))))
+                
+            } else {
+                out <- c(out, "", "Unable to compute confidence intervals and p-values.")
+            }
+            
+            
+            
         }
     }
     
@@ -80,6 +118,8 @@ inference.inzdot <- function(object, bs, width, ...) {
 }
 
 formatTriMat <- function(mat, names) {
+    ## Formats a (lower) triangular matrix nicely for display:
+    
     mat[!lower.tri(mat)] <- NA
     mat <- mat[-1, ]
     
@@ -89,12 +129,33 @@ formatTriMat <- function(mat, names) {
     mat[grep("NA", mat)] <- ""
     
     mat <- cbind(c("", names[-1]),
-                     rbind(names, mat))
+                 rbind(names, mat))
     mat <- mat[, -ncol(mat)]
     
     mat <- matrix(apply(mat, 2, function(col) {
         format(col, justify = "right")
     }), nrow = nrow(mat))
+
+    mat
+}
+
+formatMat <- function(mat, digits = 4) {
+    dn <- dimnames(mat)
+    
+    mat <- t(apply(mat, 1, function(x) suppressWarnings(as.numeric(x))))
+    mat <-  matrix(apply(mat, 2, function(col) {
+        format(col, digits = digits)
+    }), nrow = nrow(mat))
+    mat[grep("NA", mat)] <- ""
+
+    mat <- cbind(c("", dn[[1]]),
+                 rbind(dn[[2]], mat))
+    
+    mat <- matrix(apply(mat, 2, function(col) {
+        format(col, justify = "right")
+    }), nrow = nrow(mat))
+
+    mat
 }
 
 
