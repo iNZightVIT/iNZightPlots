d95dfb0563e30e2ca9ba7f59d201ec054271ec49
Tom Elliott <tom.elliott@auckland.ac.nz>
2015-10-28 09:17:14 +1300

    fix missing count in summaries

--- R/getSummary.R
+++ R/getSummary.R
@@ -74,6 +74,12 @@ getPlotSummary <- function(x, y = NULL, g1 = NULL, g1.level = NULL,
     
     ## we now want to create a data object which contains *ALL* of the necessary
     ## information, including survey design, or frequency information:
+
+    ## remove these as they aren't necessary and cause problems with "n.missing"
+    rmv <- which(names(m) %in% c("colby", "sizeby"))
+    if (length(rmv) > 0)
+        m <- m[-rmv]
+    
     if (!"df" %in% ls())
         df <- inzDataframe(m, data = md, names = varnames, g1.level, g2.level, env = env)
 
@@ -112,10 +118,9 @@ getPlotSummary <- function(x, y = NULL, g1 = NULL, g1.level = NULL,
     ## }
 
     
-
     obj <- iNZightPlot(x = x, y = y, g1 = g1, g1.level = g1.level,
                        g2 = g2, g2.level = g2.level, varnames = varnames,
-                       colby = colby, sizeby = sizeby,
+                       colby = NULL, sizeby = NULL,
                        data = data, design = design, freq = freq,
                        missing.info = missing.info, inzpars = inzpars,
                        plot = FALSE, df = df, ...)
