c201c65b283d0c1368fc3e45a72c6212b6cfd69d
Tom Elliott <tomelliottnz@gmail.com>
2016-07-05 10:11:10 +1200

    fix hex factor levels

--- R/inzhexplot.R
+++ R/inzhexplot.R
@@ -80,7 +80,8 @@ plot.inzhex <- function(obj, gen) {
         } else {
             colours <- col.args$f.cols
         }
-        hextri::panel.hextri(x = obj$x, y = obj$y, groups = as.factor(levels(obj$colby)),
+        hextri::panel.hextri(x = obj$x, y = obj$y,
+                             groups = factor(levels(obj$colby), levels = levels(obj$colby)),
                              subscripts = as.numeric(obj$colby), colours = colours,
                              nbins = obj$n.bins, style = opts$hex.style, diffuse = opts$hex.diffuse,
                              shape = convertHeight(current.viewport()$height, "in", TRUE) /
