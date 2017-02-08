b1d0b6fb36f6e27c89e9dfa9ff4e7cbd4bc0733e
Tom Elliott <tomelliottnz@gmail.com>
2017-02-09 11:20:59 +1300

    S3 templates for function

--- R/exportHTML.R
+++ R/exportHTML.R
@@ -1,4 +1,22 @@
-exportHTML <- function(plot, file) {
+exportHTML <- function(x, file) UseMethod("exportHTML")
+
+exportHTML.function <- function(x, file, width = dev.size()[1], height = dev.size()[2]) {
+  t <- tempfile()
+  pdf(t, height = height, width = width)
+  obj <- x()
+  out <- exportHTML(obj, file)
+  dev.off()
+  ## delete pdf too
+  return(out)
+}
+
+exportHTML.inzplotoutput <- function(x, file) {
+  plot <- x$all$all
+  if (is.null(plot)) {
+    warning("iNZight doesn't handle interactive panel plots ... yet!")
+    return()
+  }
+  
   dir <- tmpdir()
   curdir <- getwd()
   setwd(dir)
@@ -9,5 +27,18 @@ exportHTML <- function(plot, file) {
   ## now work with files in `dir`
   svgpath <- file.path(dir, "inzightplot.svg")
   
+  tbl <- getTable(plot)
   
+  return(htmlfile)
+}
+
+# updatePlot <- function() iNZightPlot(x, y)
+# exportHTML(updatePlot, file)
+
+
+
+getTable <- function(plot) UseMethod("getTable")
+  
+getTable.inzdot <- function(plot) {
+  # get the table from plot  
 }
