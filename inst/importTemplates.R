02f74ef758064739be1c566e796bd3d870764353
ysoh286 <ysoh286@aucklanduni.ac.nz>
2018-08-13 09:43:04 +1200

    remove code + udpate docs

--- inst/importTemplates.R
+++ inst/importTemplates.R
@@ -5,25 +5,6 @@ dirname <- "inst"
 HTMLtemplate <- readLines(file.path(dirname, 'template.html'))
 styles <- paste(readLines(file.path(dirname, 'style.css')), collapse = "\n")
 
-# store vendor/library files
-#vendorFiles <- list(jquery.min.js = "https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js",
-#                    d3.v4.min.js = "https://d3js.org/d3.v4.min.js",
-#                    jquery.dataTables.min.js = "https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js",
-#                    bootstrap.min.js = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
-#                    dataTables.bootstrap.min.js = "https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js",
-#                    dataTables.bootstrap.min.css = "https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css",
-#                    bootstrap.min.css = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css")
-#
-#vendor <- lapply(vendorFiles, function(x) {
-#  con <- url(x)
-#  text <- paste(readLines(con), collapse = "\n")
-#  close(con)
-#  return(text)
-#})
-
-#names(vendor) <- names(vendorFiles)
-#assets <- system.file("templates", "assets.zip")
-
 #load JS files
 files <- list(inzJS = "inzplot.js", bpJS = "barplot.js", dpspJS = "dpsp.js",
               histJS = "histogram.js", hexbinJS = "hexbin.js", mapsJS = "maps.js")
