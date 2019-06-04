baf12427b0c29fd4118411ec3e99ec00b4dde69f
Tom Elliott <tomelliottnz@gmail.com>
2019-06-05 09:13:03 +1200

    Fix tests

--- tests/testthat/test_poststrat.R
+++ tests/testthat/test_poststrat.R
@@ -1,13 +1,17 @@
 context("Post stratification weights")
 
-data(api)
+data(api, package = "survey")
 dclus1 <- svydesign(id = ~dnum, weights = ~pw, 
     data = apiclus1, fpc = ~fpc)
 pop.types <- data.frame(stype = c("E","H","M"), Freq = c(4421,755,1018))
 dclus1p <- postStratify(dclus1, ~stype, pop.types)
 
-load_all()
-iNZightPlot(api00, api99, design = dclus1)
-iNZightPlot(api00, api99, design = dclus1p)
+# load_all()
+# iNZightPlot(api00, api99, design = dclus1)
 
-eval(dclus1p$call[[2]])
+test_that("Post strat designs work", {
+    expect_is(
+        iNZightPlot(api00, api99, design = dclus1p),
+        "inzplotoutput"
+    )
+})
