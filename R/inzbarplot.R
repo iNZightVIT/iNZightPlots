6cc0d508b823ef9a18d6e020b64c6a96fb6790cd
Tom Elliott <tomelliottnz@gmail.com>
2019-07-04 14:57:53 +1200

    fix all na values

--- R/inzbarplot.R
+++ R/inzbarplot.R
@@ -101,7 +101,8 @@ create.inz.barplot <- function(obj) {
     # false: use phat
     ymax <-
         if (counts) max(tab, na.rm = TRUE)
-        else max(c(0, phat), na.rm = TRUE)
+        else if (all(is.na(phat))) 0
+        else max(phat, na.rm = TRUE)
     if (!is.null(ZOOM)) {
         if (ZOOM[1] <= ncol(phat)) {
             ww <- ZOOM[1]:(sum(ZOOM) - 1)
