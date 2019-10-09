2c4d00f556a7aed00698f199a038b8bdeb34a80d
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2019-10-10 11:57:02 +1300

    Add use.plotly attribute

--- R/iNZGG.R
+++ R/iNZGG.R
@@ -443,6 +443,7 @@ iNZightPlotGG <- function(
   attr(plot_object, "code") <- unname(unlist(lapply(plot_exprs, rlang::expr_text)))
   attr(plot_object, "plottype") <- c(type)
   attr(plot_object, "varnames") <- unlist(dots)
+  attr(plot_object, "use.plotly") <- !type %in%  c("gg_pie", "gg_donut", "gg_gridplot", "gg_barcode2", "gg_barcode", "gg_ridgeline")
   
   if (type %in% c("gg_lollipop", "gg_column2")) {
     attr(plot_object, "varnames") <- attr(plot_object, "varnames")[names(attr(plot_object, "varnames")) != "y"]
