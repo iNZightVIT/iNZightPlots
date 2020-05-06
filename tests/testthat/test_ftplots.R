b077a63a569f6dc9f16c7fa690598c9a3800152b
Tom Elliott <tomelliottnz@gmail.com>
2020-05-06 14:36:18 +1200

    Fix #105

--- tests/testthat/test_ftplots.R
+++ tests/testthat/test_ftplots.R
@@ -5,3 +5,26 @@ test_that("Inversing x/y doesn't affect graph", {
     p2 <- iNZightPlot(Sepal.Length, Species, data = iris, plottype = "gg_violin")
     expect_equal(attr(p1, "code"), attr(p2, "code"))
 })
+
+test_that("Y-axis label for one-way numeric is blank", {
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_barcode")
+    expect_equal(p1$labels$y, "")
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_barcode2")
+    expect_equal(p1$labels$y, "")
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_barcode3")
+    expect_equal(p1$labels$y, "")
+
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_boxplot")
+    expect_equal(p1$labels$x, "")
+
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_violin")
+    expect_equal(p1$labels$x, "")
+
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_dotstrip")
+    expect_equal(p1$labels$y, "")
+
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_beeswarm")
+    expect_equal(p1$labels$x, "")
+    p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_quasirandom")
+    expect_equal(p1$labels$x, "")
+})
