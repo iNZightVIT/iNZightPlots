36ec421af6962d46d22c4e8825fb38b60b8a14b9
Tom Elliott <tomelliottnz@gmail.com>
2020-05-27 08:53:45 +1200

    Handle numeric colby with emph

--- R/iNZightPlot.R
+++ R/iNZightPlot.R
@@ -485,21 +485,28 @@ iNZightPlot <- function(x,
                         }
                     )
                 )
-                print(opts$col.fun)
-                print(opts$plot.features)
-                print(head(df$data$colby))
                 if (opts$emph.on.top) {
                     if (is.null(opts$plot.features$order.first) ||
                         length(opts$plot.features$order.first) == 0) {
                         ## set point order so emphasised are on top (at bottom of data)
-                        ord1st <- which(as.integer(df$data$colby) == opts$col.emph)
+                        cby <- df$data$colby
+                        if (is_num(cby)) {
+                            Qs <- seq(min(cby, na.rm = TRUE), max(cby, na.rm = TRUE),
+                                length = opts$col.emphn + 1
+                            )
+                            ord1st <- which(
+                                cby >= Qs[opts$col.emph] &
+                                cby < Qs[opts$col.emph + 1]
+                            )
+                        } else {
+                            ord1st <- which(as.integer(df$data$colby) == opts$col.emph)
+                        }
                         opts$plot.features <- modifyList(
                             opts$plot.features,
                             list(order.first = ord1st)
                         )
                     }
                 }
-                print(opts$plot.features)
             }
         }
     }
--- tests/testthat/test_palettes.R
+++ tests/testthat/test_palettes.R
@@ -51,7 +51,7 @@ test_that("Emphasize works", {
 
 test_that("Emphasized points on top", {
     p <- iNZPlot(Sepal.Width ~ Sepal.Length, data = iris, colby = Species,
-        pch = 19, col.fun = inzpalette("bright"), col.emph = 2L, plot = FALSE)
+        pch = 19, col.fun = inzpalette("bright"), col.emph = 2L, plot = interactive())
     expect_equal(
         p$gen$opts$plot.features$order.first,
         which(iris$Species == unique(iris$Species)[2])
@@ -76,7 +76,7 @@ test_that("Plot function handles strings", {
         emphasize_pal_colour(3L, 2L, TRUE, 3L, inzpalette("contrast"))
     )
 
-    p <- iNZightPlot(Sepal.Width, Petal.Width, data = iris, colby = Sepal.Length,
-        col.fun = "viridis", col.emph = 2, col.emphn = 5)
+    p <- iNZightPlot(Sepal.Width, Sepal.Length, data = iris, colby = Sepal.Length,
+        col.fun = "viridis", col.emph = 2, col.emphn = 5, pch = 19)
     expect_equal(length(p$gen$col.args$n.cols), 200L)
 })
