4be339e8255b30493a58cdf06716b2bf00ef0295
Tom Elliott <tomelliottnz@gmail.com>
2020-06-30 12:35:11 +1200

    Specify CRAN

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -3,6 +3,7 @@ github_deps <- c(
     "iNZightVIT/iNZightTools@dev"
 )
 
+options(repos = c(CRAN = "https://cloud.r-project.org"))
 remotes::install_github(github_deps)
 remotes::install_deps(dependencies = TRUE)
 remotes::install_cran("rcmdcheck")
