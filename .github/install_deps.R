a232bacb1206a896dd49c0e9047dd6c61ac11878
Tom Elliott <tomelliottnz@gmail.com>
2020-07-06 16:40:01 +1200

    Attempt to install XML on R 3.6

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -5,14 +5,15 @@ github_deps <- c(
 
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
+if (.Platform$OS.type == "windows" && getRversion() < numeric_version("4")) {
+    download.file(
+        "https://cran.r-project.org/bin/windows/contrib/3.6/XML_3.99-0.3.zip",
+        "xml.zip"
+    )
+    unzip("xml.zip", .libPaths())
+    unlink("xml.zip")
+}
+
 remotes::install_github(github_deps)
-
-op <- options()
-
-if (.Platform$OS.type == "windows")
-    op <- options(install.packages.compile.from.source = "never")
-
 remotes::install_deps(dependencies = TRUE)
-
-options(op)
 remotes::install_cran("rcmdcheck")
