cb1422a553b5f2c909feaeaa201ee644355a8b1b
Tom Elliott <tomelliottnz@gmail.com>
2020-07-06 16:24:21 +1200

    Set options to install binaries on windwos

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -4,6 +4,15 @@ github_deps <- c(
 )
 
 options(repos = c(CRAN = "https://cloud.r-project.org"))
+
 remotes::install_github(github_deps)
+
+op <- options()
+
+if (.Platform$OS.type == "windows")
+    op <- options(install.packages.compile.from.source = "never")
+
 remotes::install_deps(dependencies = TRUE)
+
+options(op)
 remotes::install_cran("rcmdcheck")
