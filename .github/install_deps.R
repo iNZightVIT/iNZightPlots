316ff62905b8d63c107480053333e121cffae760
Tom Elliott <tomelliottnz@gmail.com>
2020-08-14 09:20:32 +1200

    Use RSPM repo on linux

--- .github/install_deps.R
+++ .github/install_deps.R
@@ -9,7 +9,16 @@ options(
     install.packages.compile.from.source = "never"
 )
 
-if (!requireNamespace("XML", quietly = TRUE))
+OS <- Sys.getenv("OS_TYPE")
+options(
+    repos = c(
+        if (OS == "Linux") RSPM = Sys.getenv("RSPM"),
+        CRAN = "https://cloud.r-project.org"
+    ),
+    install.packages.compile.from.source = "never"
+)
+
+if (OS != "Linux" && !requireNamespace("XML", quietly = TRUE))
     install.packages("XML", type = "binary")
 
 remotes::install_github(github_deps)
