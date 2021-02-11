027354339beb27b83266a349eb4872bc8eb50944
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-02-12 11:48:19 +1300

    Start fix for issue where barplots fail if survey subset contains all NAs (#202)

--- tests/testthat/test_survey_methods.R
+++ tests/testthat/test_survey_methods.R
@@ -35,3 +35,41 @@ test_that("Mean indicator uses correct weights", {
     )
     expect_equivalent(p$all$all$meaninfo$all$mean, svymean(~api00, dclus2))
 })
+
+
+chis <- iNZightTools::smart_read("chis.csv")
+# chis <- iNZightTools::smart_read("tests/testthat/chis.csv")
+dchis <- suppressWarnings(svrepdesign(
+    data = chis,
+    repweights = "rakedw[1-9]",
+    weights = ~rakedw0,
+    type = "other", scale = 1, rscales = 1
+))
+
+test_that("Subsetting replicate weight surveys is correct", {
+    dchis_male <- subset(dchis, sex=="male")
+    p0 <- inzplot(~race | sex, data = dchis, g1.level = "male")
+    p1 <- inzplot(~race, data = dchis_male)
+
+    expect_equal(p0$all$male$tab, p1$all$all$tab)
+})
+
+
+# ## Missing values:
+# apijk <- smart_read("apiclus2-jk1.csv")
+# # apijk <- iNZightTools::smart_read("tests/testthat/apiclus2-jk1.csv")
+
+# apides <- svrepdesign(weights = ~pw, repweights = "repw[0-9]+",
+#     data = apijk, type = "JK1")
+# svymean(~api00, des=apides)
+# svymean(~enroll, des=apides, na.rm = TRUE)
+
+# inzplot(~enroll, design = apides, inference.par="mean", inference.type="conf")
+
+# tt <- iNZightTools::import_survey("~/postdoc/data/tekupenga/TeKupenga.svydesign")
+# tt <- iNZightTools::convertToCat(tt$design, c("qTTTBeenToAncestral", "qTTTBeenToAncestral12Mths"))
+# inzplot(~qTTTBeenToAncestral12Mths.cat | qTTTBeenToAncestral.cat, design = tt)
+
+# table(tt$variables$qTTTBeenToAncestral.cat, tt$variables$qTTTBeenToAncestral12Mths.cat)
+
+# inzplot(~qTTTBeenToAncestral12Mths.cat | qTTTBeenToAncestral.cat, design = tt, g1.level = 1)
