754f97bd8f92ba79ec073a87e2e1b243071e0153
Tom Elliott <tomelliottnz@gmail.com>
2021-03-17 14:42:26 +1300

    write db to tempfile

--- tests/testthat/test_database.R
+++ tests/testthat/test_database.R
@@ -3,11 +3,13 @@ context("Database connection")
 library(RSQLite)
 library(dplyr)
 library(dbplyr)
-nhanes <- read.csv("nhanes.csv")
-con <- dbConnect(SQLite(), "nhanes.db")
-dbWriteTable(con, "nhanes", nhanes)
-unlink("nhanes")
 
+nhanes <- read.csv("nhanes.csv")
+db <- tempfile(fileext = ".db")
+on.exit(dbDisconnect(con) && unlink(db))
+con <- dbConnect(SQLite(), db)
+dbWriteTable(con, "nhanes", nhanes)
+rm("nhanes")
 
 test_that("Database connection can be graphed", {
     inzplot(~Height, con = con, data = "nhanes")
