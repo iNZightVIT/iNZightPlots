a2e26ebbb8187c1175274b6336329b04e42e161e
Tom Elliott <tom.elliott@auckland.ac.nz>
2021-03-19 12:06:18 +1300

    Possible fix for #220 - needs further testing

--- tests/testthat/test_dot.R
+++ tests/testthat/test_dot.R
@@ -81,3 +81,7 @@ test_that("Inference information is correct", {
 test_that("Dot plot with single unique value", {
     expect_silent(inzplot(~x, data = data.frame(x = rep(10, 10))))
 })
+
+test_that("Transformations for dot plots", {
+    inzplot(~Sepal.Length, data = iris, transform = list(x = "log"))
+})
