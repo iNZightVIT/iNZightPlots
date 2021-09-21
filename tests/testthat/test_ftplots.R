621d874597cf338a91b8dbfe0aa91e31879efb20
Daniel Barnett <16076593+daniel-barnett@users.noreply.github.com>
2021-09-21 12:13:17 +1200

    Add another rotation test for gridplots (different methodology than regular ggplots)

--- tests/testthat/test_ftplots.R
+++ tests/testthat/test_ftplots.R
@@ -51,6 +51,10 @@ test_that("Plots can be rotated", {
     p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_violin",
         rotation = TRUE)
     expect_false(grepl("coord_flip", attr(p1, "code")))
+    
+    p1 <- iNZightPlot(Species, data = iris, plottype = "gg_gridplot", 
+        rotation = TRUE)
+    expect_match(attr(p1, "code")[2], "flip = TRUE")
 
     # p1 <- iNZightPlot(Sepal.Length, data = iris, plottype = "gg_violin",
     #     rotation = TRUE, rotate_labels = list(x = TRUE))
